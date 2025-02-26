f6dd46c1db188e8db2618abd27ce62716364a837
Martin Cook <33993749+martycook@users.noreply.github.com>
2025-02-26 10:38:37 -0600

    I added methods for reading assembly meta-data

--- src/CodeGator.Reflection/Extensions/AssemblyExtensions.cs
+++ src/CodeGator.Reflection/Extensions/AssemblyExtensions.cs
@@ -23,7 +23,7 @@ public static partial class AssemblyExtensions
     /// <param name="assembly">The assembly to use for the search.</param>
     /// <returns>A list of matching <see cref="Type"/> objects.</returns>
     public static IEnumerable<Type> GetDecoratedTypes<T>(
-        this Assembly assembly
+        [NotNull] this Assembly assembly
         ) where T : Attribute
     {
         foreach (Type type in assembly.GetTypes())
@@ -43,27 +43,26 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's company attribute.</returns>
-    public static string ReadCompany(this Assembly assembly)
+    public static string ReadCompany(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's company attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyCompanyAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyCompanyAttribute attr || attr.Company.Length == 0)
+        if (attributes[0] is not AssemblyCompanyAttribute attr || 
+            attr.Company.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Company;
     }
 
@@ -75,27 +74,26 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's copyright attribute.</returns>
-    public static string ReadCopyright(this Assembly assembly)
+    public static string ReadCopyright(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's copyright attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyCopyrightAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyCopyrightAttribute attr || attr.Copyright.Length == 0)
+        if (attributes[0] is not AssemblyCopyrightAttribute attr || 
+            attr.Copyright.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Copyright;
     }
 
@@ -107,27 +105,26 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's title attribute.</returns>
-    public static string ReadTitle(this Assembly assembly)
+    public static string ReadTitle(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's title attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyTitleAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyTitleAttribute attr || attr.Title.Length == 0)
+        if (attributes[0] is not AssemblyTitleAttribute attr || 
+            attr.Title.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Title;
     }
 
@@ -139,27 +136,26 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's description attribute.</returns>
-    public static string ReadDescription(this Assembly assembly)
+    public static string ReadDescription(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's description attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyDescriptionAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyDescriptionAttribute attr || attr.Description.Length == 0)
+        if (attributes[0] is not AssemblyDescriptionAttribute attr || 
+            attr.Description.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Description;
     }
 
@@ -171,27 +167,26 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's product attribute.</returns>
-    public static string ReadProduct(this Assembly assembly)
+    public static string ReadProduct(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's product attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyProductAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyProductAttribute attr || attr.Product.Length == 0)
+        if (attributes[0] is not AssemblyProductAttribute attr || 
+            attr.Product.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Product;
     }
 
@@ -203,27 +198,26 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's trademark attribute.</returns>
-    public static string ReadTrademark(this Assembly assembly)
+    public static string ReadTrademark(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's trademark attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyTrademarkAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyTrademarkAttribute attr || attr.Trademark.Length == 0)
+        if (attributes[0] is not AssemblyTrademarkAttribute attr || 
+            attr.Trademark.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Trademark;
     }
 
@@ -235,28 +229,54 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's version attribute.</returns>
-    public static string ReadAssemblyVersion(this Assembly assembly)
+    public static string ReadAssemblyVersion(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's version attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyVersionAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyVersionAttribute attr || attr.Version.Length == 0)
+        if (attributes[0] is not AssemblyVersionAttribute attr || 
+            attr.Version.Length == 0)
         {
             return string.Empty;
         }
 
-        // Return the text for the attribute.
         return attr.Version;
+    }
+
+    // ******************************************************************
+
+    /// <summary>
+    /// This method returns the GIT commit hash for the given assembly.
+    /// </summary>
+    /// <param name="assembly">The assembly to use for the operation.</param>
+    /// <returns>The GIT hash for the assembly.</returns>
+    public static string ReadCommit(
+        [NotNull] this Assembly assembly
+        )
+    {
+        // This method is based on the following blog post: 
+        // https://www.hanselman.com/blog/adding-a-git-commit-hash-and-azure-devops-build-number-and-build-id-to-an-aspnet-website
+
+        var version = "1.0.0+LOCALBUILD"; // Dummy version for local dev
+
+        var infoVerAttr = assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>()
+            .FirstOrDefault();
+
+        if (infoVerAttr != null && infoVerAttr.InformationalVersion.Length > 6)
+        {
+            // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
+            version = infoVerAttr.InformationalVersion;
+        }
+        return version.Substring(version.IndexOf('+') + 1);
     }
 
     // ******************************************************************
@@ -267,43 +287,70 @@ public static partial class AssemblyExtensions
     /// </summary>
     /// <param name="assembly">The assembly to read from.</param>
     /// <returns>The value of the given assembly's informational version attribute.</returns>
-    public static string ReadInformationalVersion(this Assembly assembly)
+    public static string ReadInformationalVersion(
+        [NotNull] this Assembly assembly
+        )
     {
-        // Attempt to read the assembly's version attribute.
-        object[] attributes = assembly.GetCustomAttributes(
+        var attributes = assembly.GetCustomAttributes(
             typeof(AssemblyInformationalVersionAttribute),
             true
             );
 
-        // Did we fail?
         if (attributes.Length == 0)
         {
             return string.Empty;
         }
 
-        // Attempt to recover a reference to the attribute.
-        if (attributes[0] is not AssemblyInformationalVersionAttribute attr || attr.InformationalVersion.Length == 0)
+        if (attributes[0] is not AssemblyInformationalVersionAttribute attr || 
+            attr.InformationalVersion.Length == 0)
         {
             return string.Empty;
         }
 
-        // Look for a '+' character, which, if found, signifies the start
-        //   of semver 2.0 version info and should be removed.
         var index = attr.InformationalVersion.IndexOf('+');
-
-        // Did we find it?
         if (0 < index)
         {
-            // Strip off everything past the '+' character.
             return attr.InformationalVersion[
                 ..(attr.InformationalVersion.Length - index - 2)
                 ];
         }
         else
         {
-            // Return the text for the attribute.
             return attr.InformationalVersion;
         }
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method returns the value of the assembly's repository URL attribute.
+    /// </summary>
+    /// <param name="asm">the assembly to use for the operation.</param>
+    /// <returns>The value of the repository URL attribute, or an empty string
+    /// if no value was found.</returns>
+    public static string ReadRepositoryUrl(
+        [NotNull] this Assembly asm
+        )
+    {
+        var customAttributes = asm.GetCustomAttributes(
+            typeof(AssemblyMetadataAttribute),
+            inherit: true
+            );
+
+        if (customAttributes.Length == 0)
+        {
+            return "";
+        }
+
+        var repositoryUrlAttribute = customAttributes.OfType<AssemblyMetadataAttribute>()
+            .FirstOrDefault(x => x.Key == "RepositoryUrl");
+
+        if (repositoryUrlAttribute is null)
+        {
+            return "";
+        }
+
+        return repositoryUrlAttribute.Value ?? "";
     }
 
     #endregion
